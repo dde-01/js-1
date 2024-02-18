df2d0ecded7e65b3ec67146950ade89e37cdba8a
Pavel Taranov <docklatte@gmail.com>
2024-02-19 01:35:25 +0300

    Переменные и значения

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+/* Переменные
+var - устарело
+let 
+const - не изменяемая */
+
+/* Наименование переменных
+let userName = 'Vasya';
+let myClass8 = 8;
+let $myVar = 'var';
+let _name = 'Pavel';
+ */
+
+let a = 1;
+console.log(a);
+a = "test";
+console.log(a);
