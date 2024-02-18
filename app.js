9c00debc4eef37f36214037af44ba58e396f1657
Pavel Taranov <docklatte@gmail.com>
2024-02-19 02:23:23 +0300

    Операторы присваивания, сравнения

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-// Базовые арифметические операторы
+// Операторы присваивания
+let age = 18 + 5;
+age += 2; // age = age + 2
+age -= 3;
+age *= 2;
+age /= 2;
 
-const width = 10;
-const height = 5;
-const space = width * height;
-const newWidth = width - 4;
-const newWidth2 = width + 4;
-const division = newWidth / newWidth2;
-const volume = 2 * 2 * 2;
-const volume2 = 2 ** 3;
-console.log(volume2);
+age++; // age = age + 1
+age--; // age = age - 1
 
-// Строки
+console.log(age);
 
-const city = "Moscow";
-const street = "Новослободская";
-const home = 5;
-console.log(city + ", " + street + " " + home);
+// Операторы сравнения
+const vasia = 20;
+console.log(age > vasia);
+console.log(age >= vasia);
+console.log(age < vasia);
+console.log(age <= vasia);
+console.log(age == vasia);
