9144a421fdba1fafd86e751adb277ad468c98b28
Pavel Taranov <docklatte@gmail.com>
2024-02-20 00:46:01 +0300

    Порядок операторов

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
-// Операторы присваивания
-let age = 18 + 5;
-age += 2; // age = age + 2
-age -= 3;
-age *= 2;
-age /= 2;
+// Порядок операторов. Выполняется от большего к меньшему
 
-age++; // age = age + 1
-age--; // age = age - 1
+const isSuited = 100 - 10 > 90 - 5;
+console.log(isSuited);
 
-console.log(age);
+// 100 - (11) 10 > (9) 90 - 5
 
-// Операторы сравнения
-const vasia = 20;
-console.log(age > vasia);
-console.log(age >= vasia);
-console.log(age < vasia);
-console.log(age <= vasia);
-console.log(age == vasia);
+const a = (6 + 10) / 2;
+console.log(a);
+
+let b;
+let c;
+c = b = 100 + 50 + 30;
+console.log(c);
+console.log(b);
