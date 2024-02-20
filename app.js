d0bc4351310e5c2b7d03fff45a193a23dc786a46
Pavel Taranov <docklatte@gmail.com>
2024-02-21 01:34:19 +0300

    Switch

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
-// Операторы равенства
-// == не строгое равенство, когда проверяются только данные
-// === строгое, когда проверяются данные и тип
+// Switch отлично использовать когда имеем много вариантов он проверяет всегда строгое равенство
 
-const secretNumber = "7";
+const role = prompt("login:");
 
-if (secretNumber === 7) {
-  console.log("Угадалго строго");
+if (role === "manager") {
+  console.log("Manager");
+} else if (role === "admin") {
+  console.log("Admin");
+} else if (role === "ceo") {
+  console.log("CEO");
+} else {
+  console.log("not authorized");
 }
 
-if (secretNumber == 7) {
-  console.log("Угадал не строго");
+switch (role) {
+  case "manager":
+    console.log("Manager");
+    break;
+  case "admin":
+    console.log("Admin");
+    break;
+  case "ceo":
+    console.log("CEO");
+    break;
+  default:
+    console.log("not authorized");
+}
+switch (role) {
+  case "manager":
+  case "admin":
+    console.log("Не руководитель");
+    break;
+  case "ceo":
+    console.log("Руководитель");
+    break;
+  default:
+    console.log("not authorized");
 }
 
-const q = prompt("Введите число");
-if (q == 7) {
-  console.log("Угадал не стого");
+const num = 1;
+switch (true) {
+  case num > 0:
+    console.log("Положительный");
+    break;
+  case num < 0:
+    console.log("Отрицательный");
+    break;
+  default:
+    console.log("Ноль!");
 }
