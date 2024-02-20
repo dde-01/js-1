9bc2ce190bed06667b371940dfc9706bc4802592
Pavel Taranov <docklatte@gmail.com>
2024-02-21 02:29:56 +0300

    Упражнение: Проверка робота

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-const bmwX3 = 100000;
-const fordFocusPrice = 10000;
-const budget = 20000;
-
-let message;
-
-if (budget > bmwX3) {
-  message = "BMW";
-} else if (budget > fordFocusPrice) {
-  message = "Ford";
-} else {
-  message = "Ты нищий";
+const res = prompt("Сколько будет 7 + или - 15?");
+switch (true) {
+  case res === "Я не робот":
+  case Number(res) === 22:
+  case Number(res) === -8:
+    console.log("Успех");
+    break;
+  default:
+    console.log("Вы робот!");
 }
-// Тернарные операторы
-10 > 0 ? console.log("Больше 0") : console.log("Не больше");
-
-console.log(`Я хочу купить ${message}`);
