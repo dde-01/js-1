304a04cdec4265a2e23c6ad29670a5a2dce9cf14
Pavel Taranov <docklatte@gmail.com>
2024-02-20 03:04:00 +0300

    False из других типов

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-// Преобразование типов
-
-const age = "18";
-console.log(Number(age) + 5);
-
-const _age = 19;
-console.log(String(_age) + age);
-
-// Если js видит + производит конкатенацию если - / * то математическую операцию
-
-const userName = "Вася";
-console.log(Number(userName) + 5);
-console.log(typeof NaN);
-console.log(Boolean(0)); // Все числа не явл. 0 будут true
-console.log(Boolean("ssssdf"));
+console.log(Boolean(0));
 console.log(Boolean(""));
-
-const a = 2 + "10";
-console.log(a - 10);
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(Number("sx")));
